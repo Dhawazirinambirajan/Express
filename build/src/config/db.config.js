ee04a0c6066280fbb674496ee87a1127794b1b7a
Dhawaziri U N <dhawaziri@gmail.com>
2024-09-03 12:09:38 +0530

    db.config.ts file modified

--- build/src/config/db.config.js
+++ build/src/config/db.config.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "123456",
-    DB: "testdb"
+    PASSWORD: "",
+    DB: "tutorialmysql"
 };
